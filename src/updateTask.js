0a6412659ac2606b93254d16cc9d40c45bcb12c0
ejespinoza5 <ejespinoza5@espe.edu.ec>
2024-12-04 08:42:05 -0500

    2 commit

---
+++ src/updateTask.js
@@ -0,0 +1,22 @@
+const AWS = require('aws-sdk');
+exports.updateTask = async (event) => {
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+    const { id } = event.pathParameters;
+    const {done,title,description}= JSON.parse(event.body);
+    await dynamodb.update({
+        TableName: 'TaskTable',
+        Key: {id,},
+        UpdateExpression: "set done = :done, title = :title, description = :description" ,
+        ExpressionAttributeValues: {
+            ":done": done,
+            ":title": title,
+            ":description": description,
+        },
+        ReturnValues: "ALL_NEW",
+    }).promise();
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({message:"Datos actualizados correctamente"}),
+    };
+}
